ab65571e094322133eafe9171e5d8d01cc68cb02
TemaKrisa <btsi1@outlook.com>
2022-05-19 14:44:32 +0300

    ver 4
    add authorization

--- DogShowProgram/Properties/Resources.Designer.cs
+++ DogShowProgram/Properties/Resources.Designer.cs
@@ -9,7 +9,7 @@
 //------------------------------------------------------------------------------
 
 
-namespace DogShowWPF.Properties
+namespace DogShowProgram.Properties
 {
     /// <summary>
     ///   A strongly-typed resource class, for looking up localized strings, etc.
@@ -43,7 +43,7 @@ namespace DogShowWPF.Properties
             {
                 if ((resourceMan == null))
                 {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("DogShowWPF.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("DogShowProgram.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
--- DogShowProgram/Windows/Authorization_Window.xaml.cs
+++ DogShowProgram/Windows/Authorization_Window.xaml.cs
@@ -10,19 +10,24 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace DogShowWPF
+namespace DogShowProgram.Windows
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for Authorization_Window.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class Authorization_Window : Window
     {
-        public MainWindow()
+        public Authorization_Window()
         {
             InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            Pages.Authorization_Page page = new Pages.Authorization_Page();
+            Frame_main.Navigate(page);
         }
     }
 }
